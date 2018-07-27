a2fc29e606f5eb9e5caa67c25088da17b42377b7
mosaxiv <mosaxiv@gmail.com>
2018-07-27 14:12:05 +0900

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const os = require('os');
+const path = require('path');
+const fs = require('fs-extra');
+const _ = require('lodash');
+const basePackageJson = require('./templates/_base/package');
+
+const preset = process.argv[2];
+
+if (preset === 'react') {
+    createPackageJson(_.merge(basePackageJson, require('./templates/react/package')));
+    fs.copySync(path.join(__dirname, './templates/_base/assets'), path.resolve('./assets'));
+    copyTemplate(preset)
+}
+
+if (preset === 'vue') {
+    createPackageJson(_.merge(basePackageJson, require('./templates/vue/package')));
+    fs.copySync(path.join(__dirname, './templates/_base/assets'), path.resolve('./assets'));
+    copyTemplate(preset)
+}
+
+function createPackageJson(packageJson) {
+    fs.writeFileSync(
+        path.resolve('package.json'),
+        JSON.stringify(packageJson, null, 2) + os.EOL
+    );
+}
+
+function copyTemplate(preset) {
+    fs.copySync(
+        path.join(__dirname, `./templates/${preset}/webpack.mix.js`),
+        path.resolve('./webpack.mix.js')
+    );
+
+    fs.copySync(
+        path.join(__dirname, `./templates/${preset}/js`),
+        path.resolve('./assets/js'));
+}
---
+++ templates/react/webpack.mix.js
@@ -0,0 +1,15 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.react('assets/js/app.js', 'public/js')
+    .sass('assets/sass/app.scss', 'public/css');
