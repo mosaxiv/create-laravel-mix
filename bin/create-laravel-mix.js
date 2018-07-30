84034abe90f0f986efbd88cab21301a3a6b2f051
mosaxiv <mosaxiv@gmail.com>
2018-07-31 01:35:26 +0900

    add commander

--- bin/create-laravel-mix.js
+++ bin/create-laravel-mix.js
@@ -5,17 +5,37 @@ const os = require('os');
 const path = require('path');
 const fs = require('fs-extra');
 const _ = require('lodash');
+const commander = require('commander');
+const chalk = require('chalk');
+
+let preset;
+
+commander
+    .version(require('../package').version)
+    .arguments('<preset>')
+    .usage(`${chalk.green('preset(vue|react)')}`)
+    .action(name => {
+        preset = name;
+    })
+    .parse(process.argv);
+
+if (typeof preset === 'undefined' || !['vue', 'react'].includes(preset)) {
+    console.error('Please specify the preset');
+    console.log();
+    console.log(
+        `For example: ${chalk.cyan(commander.name())} ${chalk.green('react')}`
+    );
+    console.log();
+    console.log(
+        `Run ${chalk.cyan(`${commander.name()} --help`)} to see all options.`
+    );
+    process.exit(1);
+}
 
 const templateDir = path.join(__dirname, '..', 'templates');
 const basePackageJson = require(path.join(templateDir, '_base', 'package.json'));
-
-const preset = process.argv[2];
-
-if (!['vue', 'react'].includes(preset)) {
-    return;
-}
-
 const packageJson = _.merge(basePackageJson, require(path.join(templateDir, preset, 'package.json')));
+
 fs.writeFileSync(
     path.resolve('package.json'),
     JSON.stringify(packageJson, null, 2) + os.EOL
