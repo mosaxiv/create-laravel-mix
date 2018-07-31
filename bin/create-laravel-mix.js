9afaa9dc4e7f04dac6325ba2a870da0cee7c8d69
mosaxiv <mosaxiv@gmail.com>
2018-07-31 23:54:26 +0900

    $npm run format

--- bin/create-laravel-mix.js
+++ bin/create-laravel-mix.js
@@ -11,56 +11,58 @@ const chalk = require('chalk');
 let preset;
 
 commander
-    .version(require('../package').version)
-    .arguments('<preset>')
-    .usage(`${chalk.green('preset(vue|react)')}`)
-    .action(name => {
-        preset = name;
-    })
-    .parse(process.argv);
+  .version(require('../package').version)
+  .arguments('<preset>')
+  .usage(`${chalk.green('preset(vue|react)')}`)
+  .action(name => {
+    preset = name;
+  })
+  .parse(process.argv);
 
 if (typeof preset === 'undefined' || !['vue', 'react'].includes(preset)) {
-    console.error('Please specify the preset');
-    console.log();
-    console.log(
-        `For example: ${chalk.cyan(commander.name())} ${chalk.green('react')}`
-    );
-    console.log();
-    console.log(
-        `Run ${chalk.cyan(`${commander.name()} --help`)} to see all options.`
-    );
-    process.exit(1);
+  console.error('Please specify the preset');
+  console.log();
+  console.log(
+    `For example: ${chalk.cyan(commander.name())} ${chalk.green('react')}`
+  );
+  console.log();
+  console.log(
+    `Run ${chalk.cyan(`${commander.name()} --help`)} to see all options.`
+  );
+  process.exit(1);
 }
 
 const templateDir = path.join(__dirname, '..', 'templates');
 const packageJson = _.merge(
-    require(path.join(templateDir, '_base', 'package.json')),
-    require(path.join(templateDir, preset, 'package.json'))
+  require(path.join(templateDir, '_base', 'package.json')),
+  require(path.join(templateDir, preset, 'package.json'))
 );
 const cwd = process.cwd();
 
 fs.writeFileSync(
-    path.resolve(cwd, 'package.json'),
-    JSON.stringify(packageJson, null, 2) + os.EOL
+  path.resolve(cwd, 'package.json'),
+  JSON.stringify(packageJson, null, 2) + os.EOL
 );
 
 fs.copySync(
-    path.join(templateDir, '_base', 'assets'),
-    path.resolve(cwd, 'assets')
+  path.join(templateDir, '_base', 'assets'),
+  path.resolve(cwd, 'assets')
 );
 
 fs.copySync(
-    path.join(templateDir, preset, 'webpack.mix.js'),
-    path.resolve(cwd, 'webpack.mix.js')
+  path.join(templateDir, preset, 'webpack.mix.js'),
+  path.resolve(cwd, 'webpack.mix.js')
 );
 
 fs.copySync(
-    path.join(templateDir, preset, 'js'),
-    path.resolve(cwd, 'assets', 'js')
+  path.join(templateDir, preset, 'js'),
+  path.resolve(cwd, 'assets', 'js')
 );
 
 console.log();
-console.log(`Creating a new ${preset} app in ${chalk.green(path.resolve(cwd))}.`);
+console.log(
+  `Creating a new ${preset} app in ${chalk.green(path.resolve(cwd))}.`
+);
 console.log();
 console.log('You begin by typing:');
 console.log();
